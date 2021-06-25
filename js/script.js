1bf0ab6dc0beb3f7fa93675692529ca0614797f0
DhruvDutta <dhrvdutta@gmail.com>
2021-06-25 19:05:46 +0530

    analog and digital

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+document.getElementById('digital').style.transform ='translate(-50%,-50%) rotateY(-180deg)';
+let analog = true
+let deg = 1;
 function toggleClock(){
-    
+
+    document.getElementById('analog').style.transform = `rotateY(${180*deg}deg)`
+    document.getElementById('digital').style.transform =`translate(-50%,-50%) rotateY(${180*(deg-1)}deg)`;
+    deg+=1
 }
 
 setInterval(function(){
@@ -14,4 +20,5 @@ setInterval(function(){
     document.getElementById('hour').style.transform = ` translate(-50%,-100%) rotate(${(d.getHours()%12)*30 + d.getMinutes()*.5}deg)`
     document.getElementById('min').style.transform = ` translate(-50%,-100%) rotate(${(d.getMinutes())*6}deg)`
     document.getElementById('sec').style.transform = ` translate(-50%,-100%) rotate(${(d.getSeconds())*6}deg)`
+    document.getElementById('time').innerHTML = `<div>${d.toLocaleTimeString()}</div><div>${d.toLocaleDateString('en-us',{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</div>`
 },500)
